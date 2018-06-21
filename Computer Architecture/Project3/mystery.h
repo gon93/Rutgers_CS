3458b1b0cc53c9a0ac87bc73152b6bf02bf3c909
gon93 <32226461+gon93@users.noreply.github.com>
2018-06-21 15:01:52 +0900

    Update mystery.h

--- Computer Architecture/Project3/mystery.h
+++ Computer Architecture/Project3/mystery.h
@@ -1,6 +1,9 @@
 /*
-*mystery.h
-*Seong Kon Kim
+//
+//Seong Kon Kim
+//Nov 6,2016
+//mystery.h
 */
+
 int dothething(int,int[]); // doing the fibonachi sequence and storing the value.
 int add(int,int); //adding 2values together
