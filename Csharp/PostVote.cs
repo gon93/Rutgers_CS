e8c8ef58b8bedfe05317e75297d04abe45952542
Seong Kon Kim <ksteve1204@gmail.com>
2018-06-26 11:40:42 +0900

    Program.cs

---
+++ Csharp/PostVote.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+
+namespace Post
+{
+    class PostVote
+    {
+        private int vote = 0;
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime Date { get; private set; }
+
+        public PostVote(DateTime date, string title, string description)
+        {
+            Date = date;
+            Title = title;
+            Description = description;
+        }
+
+        
+
+        public int UpVote()
+        {
+            vote++;
+            return vote;
+        }
+        public int DownVote()
+        {
+            vote--;
+            return vote;
+        }
+        public int Vote
+        {
+            get { return vote;}
+        }
+    }
+}
---
+++ Csharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Create a Post:");
+            Console.WriteLine("1:Type Title");
+            var title = Console.ReadLine();
+            Console.WriteLine("2:Type Description");
+            var description = Console.ReadLine();
+            var post1 = new PostVote(DateTime.Now,title,description);
+            post1.UpVote();
+            post1.DownVote();
+            post1.UpVote();
+            post1.UpVote();
+            post1.UpVote();
+            post1.UpVote();
+            post1.UpVote();
+
+            Console.WriteLine("\nTitle: {0} \nDescription\n{1} \n{2}\nLikes: {3}",post1.Title,post1.Description,post1.Date,post1.Vote);
+        }
+    }
+}
