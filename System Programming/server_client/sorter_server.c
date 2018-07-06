26146edb7854dbd644767fe2ee4394a3f45c42b8
SeongKon Kim <32226461+gon93@users.noreply.github.com>
2018-07-06 09:41:15 +0900

    Update sorter_server.c

--- System Programming/server_client/sorter_server.c
+++ System Programming/server_client/sorter_server.c
@@ -13,38 +13,6 @@
 /* Pthread Library */
 #include <pthread.h>
 #include "sorter_server.h"
-int o;
-int i;
-int iparryc=0;
-int thesize=50000000;
-int ttid_size=10000;/*the size of setting the int* and [thread-t* */
-int ttid_cnt=0;//the cnt idex of ttid
-int c_data_size = 10000;
-int* arrayosocketfd; //array that holdsthe c_socket 
-pthread_t* ttid;//array that hold tid
-char** singleclient;
-int singleclientcounter=0;
-//client dat.saving
-char** c_data;
-int row_num = 0;
-char** iparry;
-int theheader=0;
-int* iparryse;
-pthread_mutex_t connection_lock;/*name of lock*/
-void* connection_handler(void *arguments);
-int print_ip = 0;
-int ip_count = 0;
-
-//Imported stuff
-FSRow*pointers[1000];	//pointers to rows, one pointer per file's worth of rows. Will only traverse through FilesToSort rows
-void sort(char** raw,char*secondArg,int c_sock  );
-//pthread_mutex_t m;
-int FilesToSort=0;	//number of files we are actually sorting (used to traverse pointers)
-int sizeRemaining[1000];//number of additions to the main file remaining for each pointer in pointers
-int universalType=1;	//sort on string or numbers?	0 = String, 1 = number;
-char*header;
-
-//End Imported Stuff
 
 
 
